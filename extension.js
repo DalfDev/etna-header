9c7e248a25175c3f884f1676c530b9c3371a1d83
Cosmonaute Cat <chevre_a@etna-alternance.net>
2019-10-28 15:28:39 +0100

    Work hard

--- extension.js
+++ extension.js
@@ -5,6 +5,12 @@ const vscode = require('vscode');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
+function convertDate(inputFormat) {
+    function pad(s) { return (s < 10) ? '0' + s : s; }
+    var d = new Date(inputFormat)
+    return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/')
+  }
+
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -17,11 +23,46 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.etna-header', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('Adding Header !');
+
+        let editor = vscode.window.activeTextEditor;
+        let pos = editor.selection.active;
+
+		if (editor) {
+            let box = vscode.window.createInputBox();
+            box.onDidAccept(() => {
+                let login = box.value;
+                box.hide()
+
+                let box2 = vscode.window.createInputBox();
+                box2.placeholder = "Description";
+                box2.show()
+
+                box2.onDidAccept(() => {
+                    box2.hide()
+                    let desc = box2.value;
+                    let ext = editor.document.uri.fsPath;
+                    vscode.window.showInformationMessage(ext);
+                    editor.edit(editBuilder => {
+                        editBuilder.insert(
+                            new vscode.Position(pos.line, pos.character + 1),
+                            "/*\n\
+** ETNA PROJECT, " + convertDate(Date()) +" by " + login + "\n\
+** " + vscode.workspace.rootPath + "\n\
+** File description:\n\
+**      " + desc + "\n\
+*/\n"
+                        );
+                    });
+                })
+            })
+            box.placeholder = "Login"
+            box.show()
+		}
 	});
 
 	context.subscriptions.push(disposable);
