b7e1dc56c3aec47b78ff5f4b09a9d58dd9f5cfb5
Cosmonaute Cat <chevre_a@etna-alternance.net>
2019-10-29 10:52:57 +0100

    OK

--- extension.js
+++ extension.js
@@ -79,7 +79,7 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-        vscode.window.showInformationMessage('Adding Header !');
+        vscode.window.showInformationMessage('Remove Etna Login');
         context.globalState.update("login", undefined);
 	});
 
